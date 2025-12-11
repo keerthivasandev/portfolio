962c600cd7ac0ab9a69b81069bd958c4450ae7a1
Keerthivasan K <keerthivasan.k@finopsly.com>
2025-12-11 21:43:09 +0530

    Buttons are added unstable and updated

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,6 +2,11 @@
 import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+
 export function Intro() {
   return (
     <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
@@ -47,7 +52,41 @@ export function Intro() {
         <span className="font-bold">3 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-      </motion.p>   
+      </motion.p>
+
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />{" "}
+        </Link>
+        <a
+          href="/CVpdf"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10"
+          download
+        >
+          Download CV {" "}<HiDownload className="opacity-60 group-hover:translate-y-1 transition"/>
+        </a>
+        <a
+          href=""
+          className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href=""
+          className="bg-white text-gray-700 p-4 flex items-center gap-2 text-[1.35rem] rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
